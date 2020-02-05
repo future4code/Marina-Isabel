109765449d7bf01e3e119068ccc543d411a60582
marinaisabels <marinaisabels@live.com>
2020-02-05 16:54:29 -0300

    Exercício 2

---
+++ semana4/classes/blog/blogfuncao.js
@@ -0,0 +1,20 @@
+function criar(){
+	const titulo = document.getElementById("titulo")
+	const autor = document.getElementById("autor")
+	const conteudo = document.getElementById("conteudo")
+	console.log("")
+}
+
+class Post {
+    constructor(titulo, autor, conteudo){
+        this.titulo = titulo
+        this.autor = autor
+        this.conteudo = conteudo
+        
+       
+    }
+}
+
+function novoPost(){
+	const sectionNovo = document.getElementBy
+}
