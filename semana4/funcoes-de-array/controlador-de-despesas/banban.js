d6c74514aacd7c9aaa7cb17fb2ff4c8cd0125011
marinaisabels <marinaisabels@live.com>
2020-02-06 16:45:14 -0300

    Metade dos Exercícios para entrega

---
+++ semana4/funcoes-de-array/controlador-de-despesas/banban.js
@@ -0,0 +1,13 @@
+class despesas{
+	construtor(valor, tipo, descricao){
+		this.valor = valor 
+		this.valor = tipo
+		this.descricao = descricao
+	}
+}
+
+function Cadastrar(){
+	const cadastroDespesa = document.getElementByClass("valor").value
+	const cadastroDespesa = document.getElementByClass("tipo").value
+	const cadastroDespesa = document.getElementByClass("descricao").value
+}
