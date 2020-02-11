93eaae3cb4a5d5e723b827fd0e27b7b68321a938
Marina Isabel <marinaisabels@live.com>
2020-02-11 21:08:22 -0200

    Exercício Dados pessoais atualizado

--- semana5/dadospessoais/dadospessoais/src/App.js
+++ semana5/dadospessoais/dadospessoais/src/App.js
@@ -13,18 +13,21 @@ function App() {
 
     <div className="App">
 
-
+        <PageSection>
         <MeusDados texto="Marina Isabel" imagem="https://pbs.twimg.com/profile_images/1195331646398046210/MRNVXlEg_400x400.jpg" />
         <DadosDoMail texto="Email: marinaisabels@live.com" imagem="https://image.flaticon.com/icons/svg/1250/1250973.svg"/> 
         <DadosEndereco texto="Rua Maria Francisca da Silva, 35" imagem="https://image.flaticon.com/icons/svg/1239/1239525.svg"/>
-
+        </PageSection>
+        <PageSection>
         <BotaoVerMais  texto="Ver Mais"/>
-
+        </PageSection>
+        <PageSection>
         <ExperienciaFuture texto="Formando desenvolvedores para o futuro"/>
         <ExperienciaOutsmart texto="Criando apps incríveis para grandes clientes"/>
-
+        </PageSection>
+        <PageSection>
         <RedeSociais titulo="Linkedin" imagem="https://cdn.icon-icons.com/icons2/1099/PNG/512/1485482199-linkedin_78667.png"/>
-          
+        </PageSection>
           
 </div>
 
