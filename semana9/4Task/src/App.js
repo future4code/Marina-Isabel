2ab34b750a208970e6f018862317d6f489dcd81e
marinaisabels <marinaisabels@live.com>
2020-03-18 11:54:32 -0300

    Mudei o componente para a pagina princial

--- semana9/4Task/src/App.js
+++ semana9/4Task/src/App.js
@@ -4,7 +4,6 @@ import { create } from 'jss'
 import { MuiThemeProvider, createGenerateClassName, jssPreset } from '@material-ui/core/styles'
 import { createMuiTheme } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import FormularioTarefa, { AppContainer } from './components/FormularioTarefa'
 
 const generateClassName = createGenerateClassName()
 const jss = create({
@@ -16,14 +15,31 @@ const jss = create({
 const theme = createMuiTheme()
 
 function App() {
-	return (
-		<JssProvider jss={jss} generateClassName={generateClassName}>
-			<MuiThemeProvider theme={theme}>
-				<CssBaseline />
-				<FormularioTarefa/>
-			</MuiThemeProvider>
-		</JssProvider>
-	)
-}
+	return( 
+		<div>
+			<h1>4Task</h1>
+				<form>
+					<input type="" placeholder="O que tem que ser feito?"></input>
+					<button>Adicionar</button>
+				</form>
+
+				<ul>
+					<li>Task 1</li>
+					
+				</ul>
+				<div>
+					<button>Marcar Todas como completas</button>
+					<p>Filtros: </p>				
+					<button>Todas</button>
+					<button>Pendentes</button>
+					<button>Completas</button>
+				</div>
+					<div>
+					<button>Remover tarefas completas</button>
+					</div>
+		    </div>
+		)	
+	}
+
 
 export default App
