4ff1e7ccc64d38036e3b6fdd903528824403b298
marinaisabels <marinaisabels@live.com>
2020-05-08 15:45:09 -0300

    Tabela de Tarefas criada

--- semana16/Projeto-ToDoList/src/index.ts
+++ semana16/Projeto-ToDoList/src/index.ts
@@ -16,17 +16,34 @@ const connection = knex({
     },
 })
 
+// async function main(): Promise<void> {
+//     await connection.raw(`
+//         CREATE TABLE TodoListUser (
+//             id VARCHAR(255) PRIMARY KEY,
+//             name VARCHAR(255) NULL,
+//             nickname VARCHAR(255) UNIQUE NOT NULL,
+//             email VARCHAR(255) UNIQUE NOT NULL
+//         )
+//     `);
+//     console.log("Tabela criada com sucesso")
+// }
+
 async function main(): Promise<void> {
     await connection.raw(`
-        CREATE TABLE TodoListUser (
-            id VARCHAR(255) PRIMARY KEY,
-            name VARCHAR(255) NULL,
-            nickname VARCHAR(255) UNIQUE NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL
-        )
+    CREATE TABLE TodoListTask (
+		id VARCHAR(255) PRIMARY KEY, 
+        title VARCHAR(255) NOT NULL, 
+        description TEXT NOT NULL, 
+        status VARCHAR(255) NOT NULL DEFAULT "to_do",
+        limit_date DATE NOT NULL,
+        creator_user_id varchar(255) NOT NULL,
+        FOREIGN KEY (creator_user_id) REFERENCES TodoListUser(id)
+);
     `);
     console.log("Tabela criada com sucesso")
 }
+
+
 
 main();
 
