5740d5972750c75007d21b9596f815ead3324073
marinaisabels <marinaisabels@live.com>
2020-05-10 11:43:43 -0300

    Funções e Endpoints obrigatorios feitos

--- semana16/Projeto-ToDoList/src/index.ts
+++ semana16/Projeto-ToDoList/src/index.ts
@@ -1,7 +1,8 @@
 import knex from "knex";
 import dotenv from "dotenv";
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { AddressInfo } from "net";
+import moment from "moment";
 dotenv.config()
 
 
@@ -9,7 +10,7 @@ const connection = knex({
     client: "mysql",
     connection: {
         host: process.env.DB_HOST,
-        port:3306,
+        port: 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE_NAME,
@@ -58,25 +59,66 @@ const connection = knex({
 
 // INSERT
 const createUser = async (
-    id:string,
-    name: string, 
+    id: string,
+    name: string,
     nickname: string,
-    email: string, 
-): Promise<void> =>{
+    email: string,
+): Promise<void> => {
     await connection.insert({
-        id:id,
-        name:name,
-        nickname:nickname,
-        email:email
+        id: id,
+        name: name,
+        nickname: nickname,
+        email: email
     })
-    .into("TodoListUser")
+        .into("TodoListUser")
     console.log("Usuário criado com sucesso!")
+}
+
+const getUserById = async (id: string, nickname: string): Promise<any> => {
+    const result = await connection.select("*").from("TodoListUser").where({
+        id: id,
+        nickname:nickname
+    });
+    return result;
+}
+
+const updateUser = async (id: string, name: string, nickname: string): Promise<void> => {
+    await connection("TodoListUser")
+        .update({
+            name: name,
+            nickname: nickname,
+        })
+        .where({ id })
+};
+
+const createTask = async (
+    title: string,
+    description: string,
+    limitDate: moment.Moment,
+    creatorUserId: string,
+): Promise<void> => {
+    await connection.insert({
+        title: title,
+        description: description,
+        limitDate: limitDate,
+        creatorUserId: creatorUserId,
+    })
+        .into("TodoListTask")
+    console.log("Tarefa criada com sucesso!")
+}
+
+const getTaskById = async (id: string): Promise<any> => {
+    const result = await connection.select("*").from("TodoListTask").where({
+        id: id,
+    });
+    return result;
 }
 
 async function main(): Promise<void> {
     try {
-        await createUser("001","Astro Dev", "astrodev", "astro@dev.com")
-    }catch (err) {
+        // await createUser("002","Astro ", "ast", "astro@dev.com.br")
+        // await createTask("Criar banco dos alunos", "Devemos criar o banco dos alunos para o módulo do backed", moment("04/05/2020", "DD/MM/YYYY"), "001")
+    } catch (err) {
         console.log(err)
     }
     // console.log("Tabela criada com sucesso")
@@ -88,21 +130,111 @@ main();
 
 
 
-// const app = express()
-
+const app = express();
+app.use(express.json());
 //  CRIAR USUÁRIO
 
-// app.put("/user", async (req: Request, res:Response) => {
-//     res.send({
-//         message: ""
-//     })
-// })
+app.put("/user", async (req: Request, res: Response) => {
+    try {
+        const id = req.body.id
+        const name = req.body.name
+        const nickname = req.body.nickname
+        const email = req.body.email
 
-// const server = app.listen(process.env.PORT || 3003, () => {
-//     if (server) {
-//       const address = server.address() as AddressInfo;
-//       console.log(`Server is running in http://localhost:${address.port}`);
-//     } else {
-//       console.error(`Failure upon starting server.`);
-//     }
-//   });
+        // console.log("id: ", id)
+        // console.log("name: ", name)
+        // console.log("nickname: ", nickname)
+        // console.log("email: ", email)
+
+        await createUser(id, name, nickname, email)
+        res.status(200).send()
+    } catch (err) {
+        res.status(400).send({
+            message: err.message,
+        })
+
+    }
+
+})
+
+// PEGAR USUÁRIO PELO ID
+
+app.get("/user/:id", async (req: Request, res: Response) => {
+    try {
+        const id = req.body.id
+        const nickname = req.body.nickname
+
+        await getUserById(id, nickname)
+        res.status(200).send()
+    } catch (err) {
+        res.status(400).send({
+            message: err.message,
+        })
+    }
+})
+
+
+// EDITAR USUÁRIO
+
+app.post("/user/edit", async(req:Request, res: Response) => {
+    try {
+        const id = req.body.id
+        const name = req.body.name
+        const nickname = req.body.nickname
+
+        await updateUser(id, name, nickname)
+        res.status(200).send()
+    }catch (err) {
+        res.status(400).send({
+            message :err.message
+        })
+    }
+})
+
+// CRIAR TAREFA
+app.put("/task", async(req: Request, res:Response) => {
+    try {
+        const title = req.body.title
+        const description = req.body.description
+        const limitDate = req.body.limitDate
+        const creatorUserId = req.body.creatorUserId
+
+        await createTask(title, description, limitDate, creatorUserId)
+        res.status(200).send()
+    }catch (err) {
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+
+// PEGAR TAREFA PELO ID
+app.get("/task/:id", async(req: Request,res: Response) => {
+    try {
+        const id = req.body.id
+        const taskId= req.body.taskId
+        const title = req.body.title
+        const description= req.body.description
+        const limitDate = req.body.limitDate
+        const status = req.body.status
+        const creatorUserId = req.body.creatorUserId
+        const creatorUserNickName = req.body.creatorUserNickName
+
+        await getTaskById(id)
+        res.status(200).send()
+    }catch (err) {
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+        console.error(`Failure upon starting server.`);
+    }
+});
