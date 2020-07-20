aac95221b09f35e6ab19f471fadf8a6ee58e138e
marinaisabels <marinaisabels@live.com>
2020-07-20 10:44:07 -0300

    Organização

---
+++ logic-exercises/ex4.js
@@ -0,0 +1,10 @@
+function strNumber() {
+    var str = "tecnologia";
+    if (str !== "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+console.log(strNumber());
---
+++ logic-exercises/ex4.ts
@@ -0,0 +1,13 @@
+
+
+function strNumber() {
+    let str = "tecnologia"
+
+    if (str !== "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+        return false
+    }else {
+        return true
+    }
+}
+
+console.log(strNumber())
