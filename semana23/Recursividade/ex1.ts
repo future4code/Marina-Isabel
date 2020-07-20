db4c9231aaff1b58f5f1e0a83ea7f30666aa81b6
marinaisabels <marinaisabels@live.com>
2020-07-20 11:16:25 -0300

    Exercícios Completos

---
+++ semana23/Recursividade/ex1.ts
@@ -0,0 +1,18 @@
+//a 
+
+const num = (n: number) => {
+    if(n >=0){
+        //num (n-1)
+        console.log(n)
+    }
+}
+
+//b 
+
+const num2 = (n: number) => {
+    if( n >=0){
+        console.log(n)
+        //num2 (n-1)
+    }
+}
+
